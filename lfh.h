0654e094b3942604b476e8be40d00ed6e9e294f5
zhaojy20 <72371740+zhaojy20@users.noreply.github.com>
2020-10-05 20:04:29 +0800

    Add files via upload

--- lfh.h
+++ lfh.h
@@ -21,9 +21,13 @@
 
 typedef struct myListCell myListCell;
 
+// To avoid palloc() appearing in this block, it's convinient to define new structure as myXXX.
+// Not like postgres structure XXX use palloc() to allocate the memory, the structure myXXX use malloc().
+// The reason is the memory allocated by palloc() would be free after each query executed.
+// But if we want to learn from the history, some data must maintain during the whole period.
 typedef struct myList {
-	NodeTag type;
-	int	length;
+	NodeTag type; // The type is T_myList
+	int	length; // The length of the List
 	myListCell* head;
 	myListCell* tail;
 }myList;
@@ -32,9 +36,24 @@ struct myListCell {
 	void* data;
 	myListCell* next;
 };
+// In order to label which base relation this variate from, we have to replace varno by relid in myVar.
+/* The definition of structure Var shows below
+	typedef struct Var
+	{
+		Expr		xpr;
+		Index		varno;			index of this var's relation in the range table, or INNER_VAR/OUTER_VAR/INDEX_VAR
+		AttrNumber	varattno;		attribute number of this var, or zero for all attrs ("whole-row Var")
+		Oid			vartype;		pg_type OID for the type of this var
+		int32		vartypmod;		pg_attribute typmod value
+		Oid			varcollid;		OID of collation, or InvalidOid if none
+		Index		varlevelsup;	for subquery variables referencing outer relations; 0 in a normal var, >0 means N levels up
+		Index		varnoold;		original value of varno, for debugging
+		AttrNumber	varoattno;		original value of varattno
+		int			location;		token location, or -1 if unknown
+	} Var; */
 typedef struct myVar {
 	Expr xpr;
-	unsigned int relid;
+	unsigned int relid; // The absolute label for base relation. The relids of base relation are consistent after they were built.
 	AttrNumber	varattno;
 	Oid			vartype;
 	int32		vartypmod;
@@ -44,45 +63,75 @@ typedef struct myVar {
 	AttrNumber	varoattno;
 	int			location;
 }myVar;
+// The attribute "constvalue" in structure Const may be different even if the corresponding part in the SQL is same, so we need to do some adaptation in structure myConst.
+/* The definition of structure Const shows below
+typedef struct Const
+{
+Expr xpr;
+Oid consttype;	 pg_type OID of the constant's datatype
+int32 consttypmod;	typmod value, if any
+Oid constcollid;	OID of collation, or InvalidOid if none
+int constlen;		typlen of the constant's datatype
+Datum constvalue;		the constant's value
+bool constisnull;	whether the constant is null (if true, constvalue is undefined)
+bool constbyval;		whether this datatype is passed by value. If true, then all the information is stored in the Datum. If false, then the Datum contains a pointer to the information.
+int location;		token location, or -1 if unknown
+} Const; */
 typedef struct myConst {
 	Expr xpr;
 	Oid consttype;
 	int32 consttypmod;
 	Oid constcollid;
 	int constlen;
-	Datum constvalue;
+	Datum constvalue; // If the corresponding part in SQL is text, do hash and save the hash value; In other condition, save the original value.
 	bool constisnull;
 	bool constbyval;
 	int location;
 }myConst;
 /* RestrictClause
-*  any condition after WHERE.
+*  Every predicates after "WHERE" clause are turned into this structure.
 */
 typedef struct RestrictClause {
-	NodeTag type;
-	NodeTag rc_type;
-	Expr* left;
-	Expr* right;
+	// Consider a predicate like "A operator B"
+	NodeTag type; // The type is T_RestrictClause
+	NodeTag rc_type; // Store operator
+	Expr* left; // Store A
+	Expr* right; // Store B
 }RestrictClause;
-/* SubRel
-*  Present for Base Tables maybe. Need more consideration.
+/* sturct myRelInfo
+*  The entity of relations, including base relation and temporary relation.
+*  If the relation is a base relation, relid is not 0, childRelList and joininfo is NULL, while RestrictClauseList may not be NULL.
+*  If the relation is a temporary relation, relid equal to 0, RestrictClauseList is NULL, but childRelList and joininfo is not NULL.
 */
 typedef struct myRelInfo {
-	NodeTag type;
-	unsigned int relid;
-	myList* RestrictClauseList;//list of baserestrict clause(RestrictClause*), like "A.age > 15 AND A.salary > 1500".
-	myList* childRelList;//table previous join
-	myList* joininfo;
+	//Consider C = A join B, A and B are base relation and C is a temporary relation
+	NodeTag type; // The type is T_myRelInfo
+	unsigned int relid; // If the relation is a base relation, relid is not 0; If the relation is a temporary relation, relid equal to 0.
+	myList* RestrictClauseList; // List of baserestrict clause(RestrictClause*), like "A.age > 15 AND A.salary > 1500".
+	myList* childRelList; // The table previous join, like A->B
+	myList* joininfo; // The join predicate between childRel
 }myRelInfo;
+/* struct History
+*  If a temporary relation appears first time, it will be recorded as a History when the SQL is executing.
+*  If the temporary relation have already appeared in the previous query, we return the selectivity to help caculating the tuples more accurate.
+*  The value of attribute "selec" will be assigned after the SQL query is executed.
+*/
 typedef struct History {
-	NodeTag type;
-	myRelInfo* content;
-	Selectivity selec;
+	//Consider C = A join B
+	NodeTag type; // The type is T_History
+	myRelInfo* content; //The entity of this relation
+	Selectivity selec; // The selectivity
 }History;
-bool my_equal(void* a, void* b);
-void* my_copyVar(PlannerInfo* root, const Var* from);
-void* my_copyConst(PlannerInfo* root, const Const* from);
-void initial_myRelInfoArray(PlannerInfo* root);
-bool learn_from_history(PlannerInfo* root, RelOptInfo* joinrel, RelOptInfo* outter_rel, RelOptInfo* inner_rel, List* joininfo, Selectivity* selec);
-int learnSelectivity(QueryDesc* queryDesc, PlanState* planstate);
+//comapre a and b. If same, return true.
+bool my_equal(const void* a, const void* b); 
+// make a copy of from.
+void* my_copyVar(const PlannerInfo* root, const Var* from); 
+// make a copy of from.
+void* my_copyConst(const PlannerInfo* root, const Const* from);
+/* Because we always add same base relation into different temporary relations' childRelList, making an array of base relations let us don't need to substantialize base relation each time during a SQL query */
+void initial_myRelInfoArray(const PlannerInfo* root);
+/* When opimizer meet a temporary relation check if we meet it before. If so return the true selectivity, if not, add the relation into a list of history */
+bool learn_from_history(const PlannerInfo* root, const RelOptInfo* joinrel, const RelOptInfo* outter_rel, const RelOptInfo* inner_rel, const List* joininfo, Selectivity* selec);
+/* Get the true selectivity after executor */
+int learnSelectivity(const QueryDesc* queryDesc, const PlanState* planstate);
 #endif
